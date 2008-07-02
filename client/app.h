51f51806af0b264b857956a9afdb926175da73a0
nicolas.alvarez <nicolas.alvarez@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2008-07-02 15:26:22 +0000

    Fix build errors and warnings on Windows.

--- client/app.h
+++ client/app.h
@@ -31,10 +31,10 @@
 //(we don't need to include the full declarations from client_state.h)
 class CLIENT_STATE;
 class PROJECT;
-class WORKUNIT;
-class RESULT;
-class APP_VERSION;
-class FILE_REF;
+struct WORKUNIT;
+struct RESULT;
+struct APP_VERSION;
+struct FILE_REF;
 class FILE_INFO;
 
 typedef int PROCESS_ID;
