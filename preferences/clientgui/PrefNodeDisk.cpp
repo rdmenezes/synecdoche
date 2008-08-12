94db41b4fb445ef299f2783e39771b5761941e20
didactylos <didactylos@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2008-08-12 01:19:06 +0000

    Convert disk preferences to property grid.

--- preferences/clientgui/PrefNodeDisk.cpp
+++ preferences/clientgui/PrefNodeDisk.cpp
@@ -21,68 +21,60 @@
 #include "prefs.h"
 #include "ValidateNumber.h"
 #include "ValidatePercent.h"
-#include "PrefNodeBase.h"
+#include "PrefGridBase.h"
 #include "PrefNodeDisk.h"
 
 
-IMPLEMENT_DYNAMIC_CLASS(PrefNodeDisk, PrefNodeBase)
+IMPLEMENT_DYNAMIC_CLASS(PrefNodeDisk, PrefGridBase)
 
 PrefNodeDisk::PrefNodeDisk(wxWindow* parent, GLOBAL_PREFS* preferences)
-: PrefNodeBase(parent, preferences) {
+: PrefGridBase(parent, preferences) {
 
     PrefGroup* quotas = AddGroup(_("Disk Quotas"));
 
     quotas->AddPreference(new PrefValueText(this,
-        _("disk_max_used_gb"),
-        _("Use no more than"),
-        _("Gigabytes of disk space"),
+        _("Maximum disk space to use (Gigabytes)"),
         _("This option is combined with the other disk restrictions to "
-        "determine the maximum space BOINC may use for project application data. "
-        "Default 10 Gigabytes."),
+        "determine the maximum space BOINC may use for project application data."),
+        _("10 Gigabytes"),
         ValidateNumber<double>(&m_preferences->disk_max_used_gb))
     );
 
     quotas->AddPreference(new PrefValueText(this,
-        _("disk_min_free_gb"),
-        _("Leave at least"),
-        _("Gigabytes of disk space free"),
+        _("Minimum free disk space (Gigabytes)"),
         _("This option is combined with the other disk restrictions to "
-        "determine the maximum space BOINC may use for project application data. "
-        "Default 0.1 Gigabytes."),
+        "determine the maximum space BOINC may use for project application data."),
+        _("0.1 Gigabytes"),
         ValidateNumber<double>(&m_preferences->disk_min_free_gb))
     );
 
     quotas->AddPreference(new PrefValueText(this,
-        _("disk_max_used_pct"),
-        _("Use no more than"),
-        _("% of total disk space"),
+        _("Maximum percentage of disk space to use"),
         _("This option is combined with the other disk restrictions to "
-        "determine the maximum space BOINC may use for project application data. "
-        "Default 50%."),
+        "determine the maximum space BOINC may use for project application data."),
+        _("50%"),
         ValidateNumber<double>(&m_preferences->disk_max_used_pct))
     );
 
     PrefGroup* access = AddGroup(_("Disk Access"));
 
     access->AddPreference(new PrefValueText(this,
-        _("disk_interval"),
-        _("Write to disk at most every"),
-        _("seconds"),
+        _("Minimum interval between disk writes (seconds)"),
         _("This setting is not observed by all projects. Use this setting to "
         "prevent the drive spinning up unnecessarily. Also refer to your power "
-        "management settings. Default 60 seconds."),
+        "management settings."),
+        _("60 seconds"),
         ValidateNumber<double>(&m_preferences->disk_interval))
     );
 
     PrefGroup* vm = AddGroup(_("Virtual Memory"));
 
     vm->AddPreference(new PrefValueText(this,
-        _("disk_interval"),
-        _("Use no more than"),
-        _("% of page file"),
+         _("Maximum percentage of page file to use"),
         _("Restrict the amount of virtual memory that BOINC may use. This "
         "setting should be used with care if your page file size is managed by "
-        "the operating system. Default 75%."),
+        "the operating system."),
+        _("75%"),
         ValidatePercent<double>(&m_preferences->vm_max_used_frac))
     );
 }
