8bf3c2d96aea0399380dd6a05075fbd952212152
didactylos <didactylos@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2008-08-17 18:42:04 +0000

    Convert CPU preferences to property grid.

--- branches/workspaces/preferences/clientgui/PrefNodeProcessor.cpp
+++ branches/workspaces/preferences/clientgui/PrefNodeProcessor.cpp
@@ -21,36 +21,34 @@
 #include "prefs.h"
 #include "ValidateBool.h"
 #include "ValidateNumber.h"
-#include "PrefNodeBase.h"
+#include "PrefGridBase.h"
 #include "PrefNodeProcessor.h"
 
 
-IMPLEMENT_DYNAMIC_CLASS(PrefNodeProcessor, PrefNodeBase)
+IMPLEMENT_DYNAMIC_CLASS(PrefNodeProcessor, PrefGridBase)
 
 
 PrefNodeProcessor::PrefNodeProcessor(wxWindow* parent, GLOBAL_PREFS* preferences)
-: PrefNodeBase(parent, preferences) {
+: PrefGridBase(parent, preferences) {
 
     PrefGroup* limits = AddGroup(_("CPU Limits"));
 
     limits->AddPreference(new PrefValueText(this,
-        _("cpu_usage_limit"),
-        _("Use no more than"),
-        _("% of processor time"),
-        _("BOINC has a built-in throttle that can limit the CPU time used. "
+        _("Maximum percentage of processor time to use"),
+        _("The built-in throttle can limit the CPU time used. "
         "This is commonly used to reduce the risk of overheating. The throttle "
         "is coarse-grained; you may see a sawtooth profile if you graph "
-        "CPU usage. Default 100%."),
+        "CPU usage."),
+        _("100%"),
         ValidateNumber<double>(&m_preferences->cpu_usage_limit, 0, 100))
     );
 
     limits->AddPreference(new PrefValueText(this,
-        _("max_cpus"),
-        _("On multiprocessor systems, at most use"),
-        _("% of available processors"),
+        _("Percentage of processors to use"),
         _("This limit specifies the number of processors or individual "
         "processor cores as a percentage of the total. Many projects will run one "
-        "process on each permitted core. Default 100%."),
+        "process on each permitted core."),
+        _("100%"),
         ValidateNumber<double>(&m_preferences->max_ncpus_pct, 0, 100))
     );
 
@@ -60,22 +58,21 @@ PrefNodeProcessor::PrefNodeProcessor(wxWindow* parent, GLOBAL_PREFS* preferences
 
     // WARNING! Prompt is opposite sense.
     PrefValueBase* run_if_user_active = new PrefValueBool(this,
-        _("run_if_user_active"),
         _("Suspend while computer is in use"),
-        _("Use this option if you want BOINC to behave like a screensaver, "
-        "only working when you are away from your computer. Default false."),
-        ValidateBoolInverse(&m_preferences->run_if_user_active));
+        _("Use this option if you want Synecdoche to behave like a screensaver, "
+        "only working when you are away from your computer."),
+        _("No"),
+        ValidateYesNo(&m_preferences->run_if_user_active, true));
 
     m_idleTimeResume = new PrefValueText(this,
-        _("idle_time_to_run"),
-        _("Resume if computer is idle for"),
-        _("minutes"),
-        _("This option prevents BOINC from starting if you are only "
-        "away from your computer briefly. Default 3 minutes."),
+        _("Interval before resuming from idle (minutes)"),
+        _("This option prevents computation from starting if you are only "
+        "away from your computer briefly."),
+        _("3 minutes"),
         ValidateNumber<double>(&m_preferences->idle_time_to_run));
 
     run_if_user_active->Connect(
-        wxEVT_COMMAND_CHECKBOX_CLICKED,
+        wxEVT_COMMAND_COMBOBOX_SELECTED,
         wxCommandEventHandler(PrefNodeProcessor::OnRunIdleChanged),
         NULL,
         this
@@ -91,22 +88,21 @@ PrefNodeProcessor::PrefNodeProcessor(wxWindow* parent, GLOBAL_PREFS* preferences
     m_suspendIdle = (m_preferences->suspend_if_no_recent_input != 0);
 
     PrefValueBase* suspend_if_no_recent_input = new PrefValueBool(this,
-        wxEmptyString,
         _("Use power saving features"),
         _("Use this option if you don't want processing to conflict "
-        "with power saving features. Default false."),
-        ValidateBool(&m_suspendIdle));
+        "with power saving features."),
+        _("No"),
+        ValidateYesNo(&m_suspendIdle));
 
     m_idleTimeSuspend = new PrefValueText(this,
-        _("suspend_if_no_recent_input"),
-        _("Suspend if computer is idle for"),
-        _("minutes"),
+        _("Idle interval before suspending (minutes)"),
         _("This is the length of time your computer will continue "
-        "processing after it falls idle. Default 20 minutes."),
+        "processing after it falls idle."),
+        _("20 minutes"),
         ValidateNumber<double>(&m_preferences->suspend_if_no_recent_input));
 
     suspend_if_no_recent_input->Connect(
-        wxEVT_COMMAND_CHECKBOX_CLICKED,
+        wxEVT_COMMAND_COMBOBOX_SELECTED,
         wxCommandEventHandler(PrefNodeProcessor::OnSuspendIdleChanged),
         NULL,
         this
@@ -121,22 +117,32 @@ PrefNodeProcessor::PrefNodeProcessor(wxWindow* parent, GLOBAL_PREFS* preferences
 
     // WARNING! Prompt is opposite sense.
     restrict->AddPreference(new PrefValueBool(this,
-        _("run_on_batteries"),
         _("Suspend while computer is running on batteries"),
-        _("Generally, laptop users don't want BOINC to run while "
-        "on battery power. Default true."),
-        ValidateBoolInverse(&m_preferences->run_on_batteries))
+        _("Generally, laptop users don't want tasks to run while "
+        "on battery power."),
+        _("Yes"),
+        ValidateYesNo(&m_preferences->run_on_batteries, true))
     );
 }
 
 
 void PrefNodeProcessor::OnRunIdleChanged(wxCommandEvent& event) {
 
-    m_idleTimeResume->Enable(event.IsChecked());
+    wxComboCtrl* combo = (wxComboCtrl*) event.GetEventObject();
+    m_idleTimeResume->Enable(combo->GetValue().IsSameAs(_("Yes")));
 }
 
 
 void PrefNodeProcessor::OnSuspendIdleChanged(wxCommandEvent& event) {
 
-    m_idleTimeSuspend->Enable(event.IsChecked());
+    wxComboCtrl* combo = (wxComboCtrl*) event.GetEventObject();
+    bool enable = combo->GetValue().IsSameAs(_("Yes"));
+
+    if (enable) {
+        m_idleTimeSuspend->SetValue(_("20"));
+    } else {
+        m_idleTimeSuspend->SetValue(_("0"));
+    }
+    
+    m_idleTimeSuspend->Enable(enable);
 }
