2d495dbac090f9e527d9c2048a66d458797eb25a
nicolas.alvarez <nicolas.alvarez@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2008-08-24 23:35:45 +0000

    Add copyright to gui_rpc_server.C.

--- trunk/client/gui_rpc_server.C
+++ trunk/client/gui_rpc_server.C
@@ -1,5 +1,6 @@
 // This file is part of Synecdoche.
 // http://synecdoche.googlecode.com/
+// Copyright (C) 2008 Nicolas Alvarez
 // Copyright (C) 2005 University of California
 //
 // Synecdoche is free software: you can redistribute it and/or modify
