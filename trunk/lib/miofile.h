92cdbe54eff132376e224a91da7e37e30b779312
nicolas.alvarez <nicolas.alvarez@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2008-08-26 00:20:53 +0000

    Doxygen formatting of comments in mfile.h and miofile.h.

--- trunk/lib/miofile.h
+++ trunk/lib/miofile.h
@@ -24,20 +24,20 @@
 
 #include "mfile.h"
 
-// MIOFILE lets you do formatted I/O to either a FILE or a memory buffer,
-// depending on how you initialize it.
-//
-// output:
-//  init_file(): output goes to the FILE* that you specify
-//  init_mfile(): output goes to an MFILE (i.e. a memory buffer);
-//     you can call MFILE::get_buf() to get the results
-// input:
-//  init_file(): input comes from the FILE* that you specify
-//  init_buf(): input comes from the buffer you specify.
-//   This string is not modified.
-//
-// Why is this here?  Because on Windows (9x, maybe all)
-// you can't do fdopen() on a socket.
+/// MIOFILE lets you do formatted I/O to either a FILE or a memory buffer,
+/// depending on how you initialize it.
+///
+/// - output:
+///   - init_file(): output goes to the FILE* that you specify
+///   - init_mfile(): output goes to an MFILE (i.e. a memory buffer);
+///     you can call MFILE::get_buf() to get the results
+/// - input:
+///   - init_file(): input comes from the FILE* that you specify
+///   - init_buf(): input comes from the buffer you specify.
+///     This string is not modified.
+///
+/// Why is this here?  Because on Windows (9x, maybe all)
+/// you can't do fdopen() on a socket.
 
 class MIOFILE {
     MFILE* mf;
