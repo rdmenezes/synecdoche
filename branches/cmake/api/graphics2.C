e9a66ae05553c00c602de778838dc07afe2730f4
nicolas.alvarez <nicolas.alvarez@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2008-08-27 15:24:10 +0000

    Merge trunk revisions 202-204 into cmake branch (license change, lots of files modified).

--- branches/cmake/api/graphics2.C
+++ branches/cmake/api/graphics2.C
@@ -1,13 +1,13 @@
-// Berkeley Open Infrastructure for Network Computing
-// http://boinc.berkeley.edu
+// This file is part of Synecdoche.
+// http://synecdoche.googlecode.com/
 // Copyright (C) 2008 University of California
 //
-// This is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation;
-// either version 2.1 of the License, or (at your option) any later version.
+// Synecdoche is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-// This software is distributed in the hope that it will be useful,
+// Synecdoche is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 // See the GNU Lesser General Public License for more details.
