eb800d8bc0d2e3c34d1f338b5561a6aee9fda581
didactylos <didactylos@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2008-09-29 19:47:34 +0000

    Fixes related to branding. These fixes allow the manager to start the daemon.

--- clientgui/BOINCClientManager.cpp
+++ clientgui/BOINCClientManager.cpp
@@ -156,9 +156,9 @@ bool CBOINCClientManager::StartupBOINCCore() {
         return (StartBOINCService());
     }
 
-    // Append boinc.exe to the end of the strExecute string and get ready to rock
+    // Append synecd.exe to the end of the strExecute string
     strExecute.Printf(
-        wxT("\"%s\\boinc.exe\" --redirectio --launched_by_manager %s"),
+        wxT("\"%ssynecd.exe\" --redirectio --launched_by_manager %s"),
         wxGetApp().GetRootDirectory().c_str(),
         wxGetApp().GetArguments().c_str()
     );
