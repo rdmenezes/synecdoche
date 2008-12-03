8c93d2b078a3985d97b71b6dc9c3bae95605eea8
DerMeister667 <DerMeister667@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2008-12-03 16:23:24 +0000

    Use TinyXML++ for parsing cc_config.xml instead of the old BOINC parser.

--- branches/ticpp/client/log_flags.C
+++ branches/ticpp/client/log_flags.C
@@ -1,5 +1,6 @@
 // This file is part of Synecdoche.
 // http://synecdoche.googlecode.com/
+// Copyright (C) 2008 Peter Kortschack
 // Copyright (C) 2005 University of California
 //
 // Synecdoche is free software: you can redistribute it and/or modify
@@ -33,30 +34,26 @@
 #include "common_defs.h"
 #include "file_names.h"
 #include "client_msgs.h"
-#include "parse.h"
 #include "str_util.h"
 #include "filesys.h"
+#include "ticpp/ticpp.h"
 
 LOG_FLAGS log_flags;
 CONFIG config;
 
 LOG_FLAGS::LOG_FLAGS() {
-
-    memset(this, 0, sizeof(LOG_FLAGS));
-
     defaults();
 }
 
+/// Reset the log flags to their default values.
 void LOG_FLAGS::defaults() {
     // on by default
     // (others are off by default)
-    //
     task = true;
     file_xfer = true;
     sched_ops = true;
 
     // off by default; intended for developers and testers
-    //
     cpu_sched = false;
     cpu_sched_debug = false;
     rr_simulation = false;
@@ -82,51 +79,45 @@ void LOG_FLAGS::defaults() {
     checkpoint_debug = false;
 }
 
-/// Parse log flag preferences
-int LOG_FLAGS::parse(XML_PARSER& xp) {
-    char tag[1024];
-    bool is_tag;
+/// Parse log flag preferences.
+///
+/// \param[in] log_flags Pointer to the TinyXML++ Element that contains the
+///                      <log_flags>-tag.
+void LOG_FLAGS::parse(const ticpp::Element* log_flags) {
+    std::string child_name;
+    for (ticpp::Element* child = log_flags->FirstChildElement(false); child; child = child->NextSiblingElement(false)) {
+        child->GetValue(&child_name);
 
-    while (!xp.get(tag, sizeof(tag), is_tag)) {
-        if (!is_tag) {
-            msg_printf(NULL, MSG_USER_ERROR,
-               "Unexpected text %s in %s", tag, CONFIG_FILE
-            );
-            continue;
+        if      (child_name == "task")                 child->GetText(&task);
+        else if (child_name == "file_xfer")            child->GetText(&file_xfer);
+        else if (child_name == "sched_ops")            child->GetText(&sched_ops);
+        else if (child_name == "cpu_sched")            child->GetText(&cpu_sched);
+        else if (child_name == "cpu_sched_debug")      child->GetText(&cpu_sched_debug);
+        else if (child_name == "rr_simulation")        child->GetText(&rr_simulation);
+        else if (child_name == "debt_debug")           child->GetText(&debt_debug);
+        else if (child_name == "task_debug")           child->GetText(&task_debug);
+        else if (child_name == "work_fetch_debug")     child->GetText(&work_fetch_debug);
+        else if (child_name == "unparsed_xml")         child->GetText(&unparsed_xml);
+        else if (child_name == "state_debug")          child->GetText(&state_debug);
+        else if (child_name == "file_xfer_debug")      child->GetText(&file_xfer_debug);
+        else if (child_name == "sched_op_debug")       child->GetText(&sched_op_debug);
+        else if (child_name == "http_debug")           child->GetText(&http_debug);
+        else if (child_name == "proxy_debug")          child->GetText(&proxy_debug);
+        else if (child_name == "time_debug")           child->GetText(&time_debug);
+        else if (child_name == "http_xfer_debug")      child->GetText(&http_xfer_debug);
+        else if (child_name == "benchmark_debug")      child->GetText(&benchmark_debug);
+        else if (child_name == "poll_debug")           child->GetText(&poll_debug);
+        else if (child_name == "guirpc_debug")         child->GetText(&guirpc_debug);
+        else if (child_name == "scrsave_debug")        child->GetText(&scrsave_debug);
+        else if (child_name == "app_msg_send")         child->GetText(&app_msg_send);
+        else if (child_name == "app_msg_receive")      child->GetText(&app_msg_receive);
+        else if (child_name == "mem_usage_debug")      child->GetText(&mem_usage_debug);
+        else if (child_name == "network_status_debug") child->GetText(&network_status_debug);
+        else if (child_name == "checkpoint_debug")     child->GetText(&checkpoint_debug);
+        else {
+            msg_printf(NULL, MSG_USER_ERROR, "Unrecognized tag in %s: <%s>\n", log_flags->GetDocument()->Value().c_str(), child_name.c_str());
         }
-        if (!strcmp(tag, "/log_flags")) return 0;
-        if (xp.parse_bool(tag, "task", task)) continue;
-        if (xp.parse_bool(tag, "file_xfer", file_xfer)) continue;
-        if (xp.parse_bool(tag, "sched_ops", sched_ops)) continue;
-        if (xp.parse_bool(tag, "cpu_sched", cpu_sched)) continue;
-        if (xp.parse_bool(tag, "cpu_sched_debug", cpu_sched_debug)) continue;
-        if (xp.parse_bool(tag, "rr_simulation", rr_simulation)) continue;
-        if (xp.parse_bool(tag, "debt_debug", debt_debug)) continue;
-        if (xp.parse_bool(tag, "task_debug", task_debug)) continue;
-        if (xp.parse_bool(tag, "work_fetch_debug", work_fetch_debug)) continue;
-        if (xp.parse_bool(tag, "unparsed_xml", unparsed_xml)) continue;
-        if (xp.parse_bool(tag, "state_debug", state_debug)) continue;
-        if (xp.parse_bool(tag, "file_xfer_debug", file_xfer_debug)) continue;
-        if (xp.parse_bool(tag, "sched_op_debug", sched_op_debug)) continue;
-        if (xp.parse_bool(tag, "http_debug", http_debug)) continue;
-        if (xp.parse_bool(tag, "proxy_debug", proxy_debug)) continue;
-        if (xp.parse_bool(tag, "time_debug", time_debug)) continue;
-        if (xp.parse_bool(tag, "http_xfer_debug", http_xfer_debug)) continue;
-        if (xp.parse_bool(tag, "benchmark_debug", benchmark_debug)) continue;
-        if (xp.parse_bool(tag, "poll_debug", poll_debug)) continue;
-        if (xp.parse_bool(tag, "guirpc_debug", guirpc_debug)) continue;
-        if (xp.parse_bool(tag, "scrsave_debug", scrsave_debug)) continue;
-        if (xp.parse_bool(tag, "app_msg_send", app_msg_send)) continue;
-        if (xp.parse_bool(tag, "app_msg_receive", app_msg_receive)) continue;
-        if (xp.parse_bool(tag, "mem_usage_debug", mem_usage_debug)) continue;
-        if (xp.parse_bool(tag, "network_status_debug", network_status_debug)) continue;
-        if (xp.parse_bool(tag, "checkpoint_debug", checkpoint_debug)) continue;
-        msg_printf(NULL, MSG_USER_ERROR, "Unrecognized tag in %s: <%s>\n",
-            CONFIG_FILE, tag
-        );
-        xp.skip_unexpected(tag, true, "LOG_FLAGS::parse");
     }
-    return ERR_XML_PARSE;
 }
 
 static void show_flag(char* buf, bool flag, const char* flag_name) {
@@ -182,13 +173,14 @@ CONFIG::CONFIG() {
     defaults();
 }
 
+/// Reset the preferences to their default values.
 void CONFIG::defaults() {
     dont_check_file_sizes = false;
     http_1_0 = false;
     save_stats_days = 30;
     ncpus = 0;
-    max_file_xfers = MAX_FILE_XFERS;
-    max_file_xfers_per_project = MAX_FILE_XFERS_PER_PROJECT;
+    max_file_xfers = 8;
+    max_file_xfers_per_project = 2;
     suppress_net_info = false;
     disallow_attach = false;
     os_random_only = false;
@@ -204,102 +196,76 @@ void CONFIG::defaults() {
     force_auth = "default";
 }
 
-int CONFIG::parse_options(XML_PARSER& xp) {
-    char tag[1024], path[256];
-    bool is_tag;
-    std::string s;
+/// Parse preferences.
+///
+/// \param[in] log_flags Pointer to the TinyXML++ Element that contains the
+///                      <options>-tag.
+void CONFIG::parse_options(const ticpp::Element* options) {
+    std::string child_name;
+    for (ticpp::Element* child = options->FirstChildElement(false); child; child = child->NextSiblingElement(false)) {
+        child->GetValue(&child_name);
 
-    while (!xp.get(tag, sizeof(tag), is_tag)) {
-        if (!is_tag) {
-            msg_printf(NULL, MSG_USER_ERROR,
-               "Unexpected text %s in %s", tag, CONFIG_FILE
-            );
-            continue;
-        }
-        if (!strcmp(tag, "/options")) {
-            return 0;
-        }
-        if (xp.parse_int(tag, "save_stats_days", save_stats_days)) continue;
-        if (xp.parse_bool(tag, "dont_check_file_sizes", dont_check_file_sizes)) continue;
-        if (xp.parse_bool(tag, "http_1_0", http_1_0)) continue;
-        if (xp.parse_int(tag, "ncpus", ncpus)) continue;
-        if (xp.parse_int(tag, "max_file_xfers", max_file_xfers)) continue;
-        if (xp.parse_int(tag, "max_file_xfers_per_project", max_file_xfers_per_project)) continue;
-        if (xp.parse_bool(tag, "suppress_net_info", suppress_net_info)) continue;
-        if (xp.parse_bool(tag, "disallow_attach", disallow_attach)) continue;
-        if (xp.parse_bool(tag, "os_random_only", os_random_only)) continue;
-        if (xp.parse_bool(tag, "no_alt_platform", no_alt_platform)) continue;
-        if (xp.parse_bool(tag, "simple_gui_only", simple_gui_only)) continue;
-        if (xp.parse_bool(tag, "dont_contact_ref_site", dont_contact_ref_site)) continue;
-        if (xp.parse_string(tag, "alt_platform", s)) {
-            alt_platforms.push_back(s);
-            continue;
-        }
-        if (xp.parse_str(tag, "data_dir", path, sizeof(path))) {
-            if (chdir(path)) {
+        if      (child_name == "save_stats_days")            child->GetText(&save_stats_days);
+        else if (child_name == "dont_check_file_sizes")      child->GetText(&dont_check_file_sizes);
+        else if (child_name == "http_1_0")                   child->GetText(&http_1_0);
+        else if (child_name == "ncpus")                      child->GetText(&ncpus);
+        else if (child_name == "max_file_xfers")             child->GetText(&max_file_xfers);
+        else if (child_name == "max_file_xfers_per_project") child->GetText(&max_file_xfers_per_project);
+        else if (child_name == "suppress_net_info")          child->GetText(&suppress_net_info);
+        else if (child_name == "disallow_attach")            child->GetText(&disallow_attach);
+        else if (child_name == "os_random_only")             child->GetText(&os_random_only);
+        else if (child_name == "no_alt_platform")            child->GetText(&no_alt_platform);
+        else if (child_name == "simple_gui_only")            child->GetText(&simple_gui_only);
+        else if (child_name == "dont_contact_ref_site")      child->GetText(&dont_contact_ref_site);
+        else if (child_name == "max_stdout_file_size")       child->GetText(&max_stdout_file_size);
+        else if (child_name == "max_stderr_file_size")       child->GetText(&max_stderr_file_size);
+        else if (child_name == "report_results_immediately") child->GetText(&report_results_immediately);
+        else if (child_name == "start_delay")                child->GetText(&start_delay);
+        else if (child_name == "run_apps_manually")          child->GetText(&run_apps_manually);
+        else if (child_name == "alt_platform") {
+            std::string pf;
+            child->GetText(&pf);
+            alt_platforms.push_back(pf);
+        } else if (child_name == "data_dir") {
+            std::string path;
+            child->GetText(&path);
+            if (chdir(path.c_str())) {
                 perror("chdir");
                 exit(1);
             }
-            continue;
-        }
-        if (xp.parse_int(tag, "max_stdout_file_size", max_stdout_file_size)) continue;
-        if (xp.parse_int(tag, "max_stderr_file_size", max_stderr_file_size)) continue;
-        if (xp.parse_bool(tag, "report_results_immediately", report_results_immediately)) continue;
-        if (xp.parse_double(tag, "start_delay", start_delay)) continue;
-        if (xp.parse_bool(tag, "run_apps_manually", run_apps_manually)) continue;
-        if (xp.parse_string(tag, "force_auth", force_auth)) {
+        } else if (child_name == "force_auth") {
+            child->GetText(&force_auth);
             downcase_string(force_auth);
-            continue;
+        } else {
+            msg_printf(NULL, MSG_USER_ERROR, "Unrecognized tag in %s: <%s>\n", options->GetDocument()->Value().c_str(), child_name.c_str());
         }
-        msg_printf(NULL, MSG_USER_ERROR, "Unrecognized tag in %s: <%s>\n",
-            CONFIG_FILE, tag
-        );
-        xp.skip_unexpected(tag, true, "CONFIG::parse_options");
     }
-    return ERR_XML_PARSE;
 }
 
-int CONFIG::parse(FILE* f) {
-    char tag[256];
-    MIOFILE mf;
-    XML_PARSER xp(&mf);
-    bool is_tag;
+int CONFIG::parse(const std::string& cfg_file_name) {
+    try {
+        ticpp::Document doc(cfg_file_name);
+        doc.LoadFile();
+        ticpp::Element* root_node = doc.FirstChildElement();
 
-    mf.init_file(f);
-    if (!xp.parse_start("cc_config")) {
-        msg_printf(NULL, MSG_USER_ERROR, "Missing start tag in %s", CONFIG_FILE);
+        ticpp::Element* log_flags_xml = root_node->FirstChildElement("log_flags");
+        log_flags.parse(log_flags_xml);
+
+        ticpp::Element* options_xml = root_node->FirstChildElement("options");
+        parse_options(options_xml);
+        return 0;
+    } catch(ticpp::Exception& ex) {
+        msg_printf(NULL, MSG_USER_ERROR, "Error while parsing config file \"%s\": %s", cfg_file_name.c_str(), ex.what());
         return ERR_XML_PARSE;
     }
-    while (!xp.get(tag, sizeof(tag), is_tag)) {
-        if (!is_tag) {
-            msg_printf(NULL, MSG_USER_ERROR,
-               "Unexpected text %s in %s", tag, CONFIG_FILE
-            );
-            continue;
-        }
-        if (!strcmp(tag, "/cc_config")) return 0;
-        if (!strcmp(tag, "log_flags")) {
-            log_flags.parse(xp);
-            continue;
-        }
-        if (!strcmp(tag, "options")) {
-            parse_options(xp);
-            continue;
-        }
-        msg_printf(NULL, MSG_USER_ERROR, "Unparsed tag in %s: <%s>\n",
-            CONFIG_FILE, tag
-        );
-        xp.skip_unexpected(tag, true, "CONFIG.parse");
-    }
-    msg_printf(NULL, MSG_USER_ERROR, "Missing end tag in %s", CONFIG_FILE);
-    return ERR_XML_PARSE;
 }
 
 /// Read the config file.
 ///
 /// \param[in] init Set this parameter to true when reading the config file
 ///                 for the first time and false when re-reading it.
-/// \return Zero on success, ERR_FOPEN if opening the config file failed.
+/// \return Zero on success, nonzero if an error occured while parsing the
+///         config file.
 int read_config_file(bool init) {
     log_flags.defaults();
     config.defaults();
@@ -307,11 +273,6 @@ int read_config_file(bool init) {
     if (!init) {
         msg_printf(NULL, MSG_INFO, "Re-reading cc_config.xml");
     }
-    FILE* f = boinc_fopen(CONFIG_FILE, "r");
-    if (!f) {
-        return ERR_FOPEN;
-    }
-    config.parse(f);
-    fclose(f);
-    return 0;
+    int ret_val = config.parse(CONFIG_FILE);
+    return ret_val;
 }
