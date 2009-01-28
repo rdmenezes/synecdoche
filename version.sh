901b1b38d5109d963e645c0dbc86133d47c6b935
nicolas.alvarez <nicolas.alvarez@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2009-01-28 03:30:55 +0000

    Fix bug in the SVN version script that caused svnversion.cpp to be modified every time, so it got recompiled every time "make" was run.

--- version.sh
+++ version.sh
@@ -7,7 +7,7 @@ else
     revision=0;
 fi
 new_revision="extern const char* SYNEC_SVN_VERSION = $revision;";
-old_revision=`cat "$1" > /dev/null`;
+old_revision=`cat "$1" 2> /dev/null`;
 
 if test "$new_revision" != "$old_revision"; then
     echo "$new_revision" > "$1"
