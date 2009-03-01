2ebea836652d55c9ea26751626810f45a7a88fe6
DerMeister667 <DerMeister667@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2009-03-01 09:40:15 +0000

    Small bugfix for version.bat

--- trunk/version.bat
+++ trunk/version.bat
@@ -2,7 +2,7 @@
 
 rem Get the svn revision number
 svnversion > %1.tmp
-set /p _revision= < %1.tmp
+set /p _revision=< %1.tmp
 if x%_revision%==xexported set _revision=0
 if x%_revision%==x set _revision=0
 
@@ -12,12 +12,12 @@ echo extern const char* SYNEC_SVN_VERSION = "%_revision%"; > %1.tmp
 rem Read and compare the old and the new version string.
 rem If they are different replace the old one by the new string
 rem else keep the old one to prevent an extra rebuild.
-set /p _oldrevision= < %1
-if not errorlevel 0 set _oldrevision=NA
-set /p _newrevision= < %1.tmp
+set /p _oldrevision=< %1
+set _oldrevision=x%_oldrevision%
+set /p _newrevision=< %1.tmp
 set _oldrevision=%_oldrevision:"=%
 set _newrevision=%_newrevision:"=%
-if "x%_oldrevision%"=="x%_newrevision%" (
+if "%_oldrevision%"=="x%_newrevision%" (
 	del %1.tmp
 ) else (
 	move %1.tmp %1
