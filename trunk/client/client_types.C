10c617f116000a2c3651d385eace7a556c2886cf
DerMeister667 <DerMeister667@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2009-03-10 21:45:42 +0000

    Applied and improved BOINC changesets: 16338/16340/16357: client: move round-robin simulation to its own file
    TODO:
    - Update Visual Studio- and XCode-project files.
    - Testing. It shouldn't break anything, but who knows...

--- trunk/client/client_types.C
+++ trunk/client/client_types.C
@@ -301,7 +301,7 @@ int PROJECT::write_state(MIOFILE& out, bool gui_rpc) const {
             "    <rr_sim_deadlines_missed>%d</rr_sim_deadlines_missed>\n"
             "    <last_rpc_time>%f</last_rpc_time>\n"
             "    <project_files_downloaded_time>%f</project_files_downloaded_time>\n",
-            rr_sim_status.deadlines_missed,
+            rr_sim_status.get_deadlines_missed(),
             last_rpc_time,
             project_files_downloaded_time
         );
--- trunk/client/client_types.h
+++ trunk/client/client_types.h
@@ -20,8 +20,8 @@
 // client_types.C  (to write and parse it)
 // client_state.C  (to cross-link objects)
 
-#ifndef _CLIENT_TYPES_
-#define _CLIENT_TYPES_
+#ifndef CLIENT_TYPES_H
+#define CLIENT_TYPES_H
 
 #if !defined(_WIN32) || defined(__CYGWIN32__)
 #include <stdio.h>
@@ -33,6 +33,7 @@
 
 #include "common_defs.h"
 #include "md5_file.h"
+#include "rr_sim.h"
 
 #define P_LOW 1
 #define P_MEDIUM 3
@@ -158,57 +159,6 @@ struct DAILY_STATS {
     int parse(FILE*);
 };
 bool operator < (const DAILY_STATS&, const DAILY_STATS&);
-
-struct RR_SIM_PROJECT_STATUS {
-    std::vector<RESULT*>active;     ///< jobs currently running (in simulation
-    std::vector<RESULT*>pending;    ///< jobs runnable but not running yet
-    int deadlines_missed;
-
-    /// Fraction of each CPU this project will get
-    /// set in CLIENT_STATE::rr_misses_deadline();
-    double proc_rate;
-
-    double cpu_shortfall;
-
-    inline void clear() {
-        active.clear();
-        pending.clear();
-        deadlines_missed = 0;
-        proc_rate = 0;
-        cpu_shortfall = 0;
-    }
-    inline void activate(RESULT* rp) {
-        active.push_back(rp);
-    }
-    inline void add_pending(RESULT* rp) {
-        pending.push_back(rp);
-    }
-    inline bool none_active() {
-        return active.empty();
-    }
-    inline bool can_run(RESULT*, int ncpus) {
-        return (int)active.size() < ncpus;
-    }
-    inline void remove_active(RESULT* r) {
-        std::vector<RESULT*>::iterator it = active.begin();
-        while (it != active.end()) {
-            if (*it == r) {
-                it = active.erase(it);
-            } else {
-                it++;
-            }
-        }
-    }
-    inline RESULT* get_pending() {
-        if (pending.empty()) return NULL;
-        RESULT* rp = pending[0];
-        pending.erase(pending.begin());
-        return rp;
-    }
-    inline int cpus_used() {
-        return (int) active.size();
-    }
-};
 
 class PROJECT {
 public:
@@ -690,4 +640,4 @@ public:
     std::string name;
 };
 
-#endif
+#endif // CLIENT_TYPES_H
