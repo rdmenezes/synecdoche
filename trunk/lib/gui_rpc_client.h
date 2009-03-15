bb4c9b3832e995b970da61d11d336b2e95fe2e90
DerMeister667 <DerMeister667@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2009-03-15 10:17:00 +0000

    Applied BOINC changesets: - 17271: GUI RPC: add master_url and local_revision to PROJECT_CONFIG struct. The former will allow the Manager to verify project URLs - 17513	MGR: Newer versions of the server-side software publish their master url in the response to the get_project_config.php request. If it exists use it over what was specified by the user. Fixes #259

--- trunk/lib/gui_rpc_client.h
+++ trunk/lib/gui_rpc_client.h
@@ -464,6 +464,8 @@ struct PROJECT_INIT_STATUS {
 struct PROJECT_CONFIG {
     int error_num;
     std::string name;
+    std::string master_url;
+    int local_revision; // SVN revision of the server software
     int min_passwd_length;
     bool account_manager;
     bool uses_username;
