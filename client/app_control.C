8127d95604f10edbee6e9ab6197fb205ab70c896
nicolas.alvarez <nicolas.alvarez@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2009-07-10 19:41:35 +0000

    Cleanup app_control.C: declare variables near use, declare loop variables inside the for (and use size_t for containers).

--- client/app_control.C
+++ client/app_control.C
@@ -390,11 +390,9 @@ bool ACTIVE_TASK::finish_file_present() const {
 }
 
 void ACTIVE_TASK_SET::send_trickle_downs() {
-    unsigned int i;
-    ACTIVE_TASK* atp;
     bool sent;
-    for (i=0; i<active_tasks.size(); i++) {
-        atp = active_tasks[i];
+    for (size_t i=0; i<active_tasks.size(); i++) {
+        ACTIVE_TASK* atp = active_tasks[i];
         if (!atp->process_exists()) continue;
         if (atp->have_trickle_down) {
             if (!atp->app_client_shm.shm) continue;
@@ -410,13 +408,11 @@ void ACTIVE_TASK_SET::send_trickle_downs() {
 }
 
 void ACTIVE_TASK_SET::send_heartbeats() {
-    unsigned int i;
-    ACTIVE_TASK* atp;
     char buf[256];
     double ar = gstate.available_ram();
 
-    for (i=0; i<active_tasks.size(); i++) {
-        atp = active_tasks[i];
+    for (size_t i=0; i<active_tasks.size(); i++) {
+        ACTIVE_TASK* atp = active_tasks[i];
         if (!atp->process_exists()) continue;
         if (!atp->app_client_shm.shm) continue;
         sprintf(buf, "<heartbeat/>"
@@ -438,11 +434,9 @@ void ACTIVE_TASK_SET::send_heartbeats() {
 }
 
 void ACTIVE_TASK_SET::process_control_poll() {
-    unsigned int i;
-    ACTIVE_TASK* atp;
 
-    for (i=0; i<active_tasks.size(); i++) {
-        atp = active_tasks[i];
+    for (size_t i=0; i<active_tasks.size(); i++) {
+        ACTIVE_TASK* atp = active_tasks[i];
         if (!atp->process_exists()) continue;
         if (!atp->app_client_shm.shm) continue;
 
@@ -468,15 +462,13 @@ void ACTIVE_TASK_SET::process_control_poll() {
 ///
 /// \return True if at least one process has exited, false otherwise.
 bool ACTIVE_TASK_SET::check_app_exited() {
-    ACTIVE_TASK* atp;
     bool found = false;
 
 #ifdef _WIN32
     unsigned long exit_code;
-    unsigned int i;
 
-    for (i=0; i<active_tasks.size(); i++) {
-        atp = active_tasks[i];
+    for (size_t i=0; i<active_tasks.size(); i++) {
+        ACTIVE_TASK* atp = active_tasks[i];
         if (!atp->process_exists()) continue;
         if (GetExitCodeProcess(atp->pid_handle, &exit_code)) {
             if (exit_code != STILL_ACTIVE) {
@@ -506,7 +498,7 @@ bool ACTIVE_TASK_SET::check_app_exited() {
     int pid, stat;
 
     if ((pid = waitpid(0, &stat, WNOHANG)) > 0) {
-        atp = lookup_pid(pid);
+        ACTIVE_TASK* atp = lookup_pid(pid);
         if (!atp) {
             // if we're running benchmarks, exited process
             // is probably a benchmark process; don't show error
@@ -554,8 +546,6 @@ bool ACTIVE_TASK::check_max_disk_exceeded() {
 /// Check if any of the active tasks have exceeded their
 /// resource limits on disk, CPU time or memory
 bool ACTIVE_TASK_SET::check_rsc_limits_exceeded() {
-    unsigned int i;
-    ACTIVE_TASK *atp;
     static double last_disk_check_time = 0;
     bool do_disk_check = false;
     bool did_anything = false;
@@ -571,8 +561,8 @@ bool ACTIVE_TASK_SET::check_rsc_limits_exceeded() {
     if (gstate.now > last_disk_check_time + min_interval) {
         do_disk_check = true;
     }
-    for (i=0; i<active_tasks.size(); i++) {
-        atp = active_tasks[i];
+    for (size_t i=0; i<active_tasks.size(); i++) {
+        ACTIVE_TASK* atp = active_tasks[i];
         if (atp->task_state() != PROCESS_EXECUTING) continue;
         if (atp->current_cpu_time > atp->max_cpu_time) {
             msg_printf(atp->result->project, MSG_INFO,
@@ -684,11 +674,9 @@ int ACTIVE_TASK::request_reread_app_info() {
 
 /// Tell all running apps of a project to reread prefs.
 void ACTIVE_TASK_SET::request_reread_prefs(PROJECT* project) {
-    unsigned int i;
-    ACTIVE_TASK* atp;
 
-    for (i=0; i<active_tasks.size(); i++) {
-        atp = active_tasks[i];
+    for (size_t i=0; i<active_tasks.size(); i++) {
+        ACTIVE_TASK* atp = active_tasks[i];
         if (atp->result->project != project) continue;
         if (!atp->process_exists()) continue;
         atp->request_reread_prefs();
@@ -696,7 +684,7 @@ void ACTIVE_TASK_SET::request_reread_prefs(PROJECT* project) {
 }
 
 void ACTIVE_TASK_SET::request_reread_app_info() {
-    for (unsigned int i=0; i<active_tasks.size(); i++) {
+    for (size_t i=0; i<active_tasks.size(); i++) {
         ACTIVE_TASK* atp = active_tasks[i];
         if (!atp->process_exists()) continue;
         atp->request_reread_app_info();
@@ -786,26 +774,25 @@ void ACTIVE_TASK_SET::suspend_all(bool cpu_throttle) {
     }
 
     for (ACTIVE_TASK_PVEC::iterator it = active_tasks.begin(); it != active_tasks.end(); ++it) {
-        ACTIVE_TASK& at = **it;
-        if (at.task_state() != PROCESS_EXECUTING) {
+        ACTIVE_TASK* task = *it;
+        if (task->task_state() != PROCESS_EXECUTING) {
             continue;
         }
         if (cpu_throttle) {
             // If we're doing CPU throttling, don't bother suspending apps
             // that don't use a full CPU.
-            if ((at.result->project->non_cpu_intensive) || (at.app_version->avg_ncpus < 1.0)) {
+            if ((task->result->project->non_cpu_intensive) || (task->app_version->avg_ncpus < 1.0)) {
                 continue;
             }
         }
-        at.preempt(!leave_in_mem);
+        task->preempt(!leave_in_mem);
     }
 }
 
 /// Resume all currently scheduled tasks.
 void ACTIVE_TASK_SET::unsuspend_all() {
-    unsigned int i;
     ACTIVE_TASK* atp;
-    for (i=0; i<active_tasks.size(); i++) {
+    for (size_t i=0; i<active_tasks.size(); i++) {
         atp = active_tasks[i];
         if (atp->scheduler_state != CPU_SCHED_SCHEDULED) continue;
         if (atp->task_state() == PROCESS_UNINITIALIZED) {
@@ -822,11 +809,9 @@ void ACTIVE_TASK_SET::unsuspend_all() {
 
 /// Check to see if any tasks are running.
 /// called if benchmarking and waiting for suspends to happen
-bool ACTIVE_TASK_SET::is_task_executing() {
-    unsigned int i;
-    ACTIVE_TASK* atp;
-    for (i=0; i<active_tasks.size(); i++) {
-        atp = active_tasks[i];
+bool ACTIVE_TASK_SET::is_task_executing() const {
+    for (size_t i=0; i<active_tasks.size(); i++) {
+        const ACTIVE_TASK* atp = active_tasks[i];
         if (atp->task_state() == PROCESS_EXECUTING) {
             return true;
         }
@@ -838,10 +823,8 @@ bool ACTIVE_TASK_SET::is_task_executing() {
 /// This is called when the core client exits,
 /// or when a project is detached or reset
 void ACTIVE_TASK_SET::request_tasks_exit(PROJECT* proj) {
-    unsigned int i;
-    ACTIVE_TASK *atp;
-    for (i=0; i<active_tasks.size(); i++) {
-        atp = active_tasks[i];
+    for (size_t i=0; i<active_tasks.size(); i++) {
+        ACTIVE_TASK* atp = active_tasks[i];
         if (proj && atp->wup->project != proj) continue;
         if (!atp->process_exists()) continue;
         atp->request_exit();
@@ -851,10 +834,8 @@ void ACTIVE_TASK_SET::request_tasks_exit(PROJECT* proj) {
 /// Send kill signal to all app processes
 /// Don't wait for them to exit
 void ACTIVE_TASK_SET::kill_tasks(PROJECT* proj) {
-    unsigned int i;
-    ACTIVE_TASK *atp;
-    for (i=0; i<active_tasks.size(); i++) {
-        atp = active_tasks[i];
+    for (size_t i=0; i<active_tasks.size(); i++) {
+        ACTIVE_TASK* atp = active_tasks[i];
         if (proj && atp->wup->project != proj) continue;
         if (!atp->process_exists()) continue;
         atp->kill_task(false);
@@ -911,7 +892,6 @@ void ACTIVE_TASK::send_network_available() {
 /// If so parse it and return true.
 bool ACTIVE_TASK::get_app_status_msg() {
     char msg_buf[MSG_CHANNEL_SIZE];
-    double fd;
 
     if (!app_client_shm.shm) {
         msg_printf(result->project, MSG_INFO,
@@ -929,6 +909,7 @@ bool ACTIVE_TASK::get_app_status_msg() {
     }
     want_network = 0;
     current_cpu_time = checkpoint_cpu_time = 0.0;
+    double fd;
     if (parse_double(msg_buf, "<fraction_done>", fd)) {
         // fraction_done will be reported as zero
         // until the app's first call to boinc_fraction_done().
@@ -960,7 +941,6 @@ bool ACTIVE_TASK::get_app_status_msg() {
 
 bool ACTIVE_TASK::get_trickle_up_msg() {
     char msg_buf[MSG_CHANNEL_SIZE];
-    bool found = false;
     int retval;
 
     if (!app_client_shm.shm) return false;
@@ -974,22 +954,20 @@ bool ACTIVE_TASK::get_trickle_up_msg() {
         if (match_tag(msg_buf, "<have_new_upload_file/>")) {
             handle_upload_files();
         }
-        found = true;
+        return true;
     }
-    return found;
+    return false;
 }
 
 /// Check for msgs from active tasks.
 /// Return true if any of them has changed its checkpoint_cpu_time
 /// (since in that case we need to write state file)
 bool ACTIVE_TASK_SET::get_msgs() {
-    unsigned int i;
-    ACTIVE_TASK *atp;
     double old_time;
     bool action = false;
 
-    for (i=0; i<active_tasks.size(); i++) {
-        atp = active_tasks[i];
+    for (size_t i=0; i<active_tasks.size(); i++) {
+        ACTIVE_TASK* atp = active_tasks[i];
         if (!atp->process_exists()) continue;
         old_time = atp->checkpoint_cpu_time;
         if (atp->get_app_status_msg()) {
