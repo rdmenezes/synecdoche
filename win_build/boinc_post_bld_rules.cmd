a86f99dae96d0598f49aa241b30eed3c587d50dd
DerMeister667 <DerMeister667@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2009-07-25 16:11:16 +0000

    Updated Windows project files for synecdoche_depends_win_vs2005 and cleaned up boinc_post_bld_rules.cmd.

--- win_build/boinc_post_bld_rules.cmd
+++ win_build/boinc_post_bld_rules.cmd
@@ -21,64 +21,39 @@ FOR /F "usebackq delims==" %%J IN ('%2') DO set OUTPUTDIR=%%~J
 FOR /F "usebackq delims==" %%K IN ('%3') DO set PLATFORMNAME=%%~K
 FOR /F "usebackq delims==" %%L IN ('%4') DO set CONFIGNAME=%%~L
 
-if not exist %OUTPUTDIR%\dbghelp.dll (
-    echo Copying dbghelp to the output directory...
-    copy "%PROJECTROOTDIR%\win_build\installerv2\redist\Windows\%PLATFORMNAME%\dbghelp.dll" "%OUTPUTDIR%"
-)
-
-if not exist %OUTPUTDIR%\dbghelp95.dll (
-    echo Copying dbghelp95 to the output directory...
-    copy "%PROJECTROOTDIR%\win_build\installerv2\redist\Windows\%PLATFORMNAME%\dbghelp95.dll" "%OUTPUTDIR%"
-)
-
-if not exist %OUTPUTDIR%\symsrv.dll (
-    echo Copying symsrv to the output directory...
-    copy "%PROJECTROOTDIR%\win_build\installerv2\redist\Windows\%PLATFORMNAME%\symsrv.dll" "%OUTPUTDIR%"
-)
-
-if not exist %OUTPUTDIR%\symsrv.yes (
-    echo Copying symsrv.yes to the output directory...
-    copy "%PROJECTROOTDIR%\win_build\installerv2\redist\Windows\%PLATFORMNAME%\symsrv.yes" "%OUTPUTDIR%"
-)
-
-if not exist %OUTPUTDIR%\srcsrv.dll (
-    echo Copying srcsrv.dll to the output directory...
-    copy "%PROJECTROOTDIR%\win_build\installerv2\redist\Windows\%PLATFORMNAME%\srcsrv.dll" "%OUTPUTDIR%"
-)
-
 if not exist %OUTPUTDIR%\libcurld.dll (
     echo Copying libcurld to the output directory...
-    copy "%PROJECTROOTDIR%\curl\mswin\%PLATFORMNAME%\%CONFIGNAME%\bin\libcurld.dll" "%OUTPUTDIR%"
+    copy "%PROJECTROOTDIR%\..\synecdoche_depends_win_vs2005\curl\mswin\%PLATFORMNAME%\%CONFIGNAME%\bin\libcurld.dll" "%OUTPUTDIR%"
 )
 
 if not exist %OUTPUTDIR%\libcurl.dll (
     echo Copying libcurl to the output directory...
-    copy "%PROJECTROOTDIR%\curl\mswin\%PLATFORMNAME%\%CONFIGNAME%\bin\libcurl.dll" "%OUTPUTDIR%"
+    copy "%PROJECTROOTDIR%\..\synecdoche_depends_win_vs2005\curl\mswin\%PLATFORMNAME%\%CONFIGNAME%\bin\libcurl.dll" "%OUTPUTDIR%"
 )
 
 if not exist %OUTPUTDIR%\ca-bundle.crt (
     echo Copying ca-bundle.crt to the output directory...
-    copy "%PROJECTROOTDIR%\curl\ca-bundle.crt" "%OUTPUTDIR%"
+    copy "%PROJECTROOTDIR%\..\synecdoche_depends_win_vs2005\curl\ca-bundle.crt" "%OUTPUTDIR%"
 )
 
 if not exist %OUTPUTDIR%\libeay32.dll (
     echo Copying libeay32 to the output directory...
-    copy "%PROJECTROOTDIR%\openssl\mswin\%PLATFORMNAME%\%CONFIGNAME%\bin\libeay32.dll" "%OUTPUTDIR%"
+    copy "%PROJECTROOTDIR%\..\synecdoche_depends_win_vs2005\openssl\mswin\%PLATFORMNAME%\%CONFIGNAME%\bin\libeay32.dll" "%OUTPUTDIR%"
 )
 
 if not exist %OUTPUTDIR%\ssleay32.dll (
     echo Copying ssleay32 to the output directory...
-    copy "%PROJECTROOTDIR%\openssl\mswin\%PLATFORMNAME%\%CONFIGNAME%\bin\ssleay32.dll" "%OUTPUTDIR%"
+    copy "%PROJECTROOTDIR%\..\synecdoche_depends_win_vs2005\openssl\mswin\%PLATFORMNAME%\%CONFIGNAME%\bin\ssleay32.dll" "%OUTPUTDIR%"
 )
 
 if not exist %OUTPUTDIR%\zlib1d.dll (
     echo Copying zlib1d to the output directory...
-    copy "%PROJECTROOTDIR%\zlib\mswin\%PLATFORMNAME%\%CONFIGNAME%\bin\zlib1d.dll" "%OUTPUTDIR%"
+    copy "%PROJECTROOTDIR%\..\synecdoche_depends_win_vs2005\zlib\mswin\%PLATFORMNAME%\%CONFIGNAME%\bin\zlib1d.dll" "%OUTPUTDIR%"
 )
 
 if not exist %OUTPUTDIR%\zlib1.dll (
     echo Copying zlib1 to the output directory...
-    copy "%PROJECTROOTDIR%\zlib\mswin\%PLATFORMNAME%\%CONFIGNAME%\bin\zlib1.dll" "%OUTPUTDIR%"
+    copy "%PROJECTROOTDIR%\..\synecdoche_depends_win_vs2005\zlib\mswin\%PLATFORMNAME%\%CONFIGNAME%\bin\zlib1.dll" "%OUTPUTDIR%"
 )
 
 EXIT /B 0
