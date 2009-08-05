9cb939af72d5583e987a9db54a9d1ff58b0234b9
nicolas.alvarez <nicolas.alvarez@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2009-08-05 19:41:01 +0000

    Remove headers for test suites. There is less redundancy if the methods are defined inline in the class{}.

--- tests/TestStrUtil.cpp
+++ tests/TestStrUtil.cpp
@@ -15,62 +15,72 @@
 // You should have received a copy of the GNU Lesser General Public
 // License with Synecdoche.  If not, see <http://www.gnu.org/licenses/>.
 
-#include "TestStrUtil.h"
-
+#include <cppunit/TestFixture.h>
 #include <cppunit/TestAssert.h>
 #include <cppunit/extensions/HelperMacros.h>
 
 #include "lib/str_util.h"
 
-void TestStrUtil::testStripWsStdString()
+class TestStrUtil: public CppUnit::TestFixture
 {
-    std::string input;
-    input = "test";
-    strip_whitespace(input);
-    CPPUNIT_ASSERT_EQUAL(std::string("test"), input);
+    CPPUNIT_TEST_SUITE(TestStrUtil);
+    CPPUNIT_TEST(testStripWsStdString);
+    CPPUNIT_TEST(testStartsWith);
+    CPPUNIT_TEST(testEndsWith);
+    CPPUNIT_TEST_SUITE_END();
 
-    input = "test   ";
-    strip_whitespace(input);
-    CPPUNIT_ASSERT_EQUAL(std::string("test"), input);
+  public:
+    void testStripWsStdString()
+    {
+        std::string input;
+        input = "test";
+        strip_whitespace(input);
+        CPPUNIT_ASSERT_EQUAL(std::string("test"), input);
 
-    input = "   test";
-    strip_whitespace(input);
-    CPPUNIT_ASSERT_EQUAL(std::string("test"), input);
+        input = "test   ";
+        strip_whitespace(input);
+        CPPUNIT_ASSERT_EQUAL(std::string("test"), input);
 
-    input = "    test   ";
-    strip_whitespace(input);
-    CPPUNIT_ASSERT_EQUAL(std::string("test"), input);
+        input = "   test";
+        strip_whitespace(input);
+        CPPUNIT_ASSERT_EQUAL(std::string("test"), input);
 
-    input = "\r\n  test\r\n";
-    strip_whitespace(input);
-    CPPUNIT_ASSERT_EQUAL(std::string("test"), input);
-}
-void TestStrUtil::testStartsWith()
-{
-    CPPUNIT_ASSERT_EQUAL(true, starts_with("preteststring", "pre"));
-    CPPUNIT_ASSERT_EQUAL(true, starts_with("pre", "pre"));
-    CPPUNIT_ASSERT_EQUAL(false, starts_with("teststring", "pre"));
-    CPPUNIT_ASSERT_EQUAL(false, starts_with("long", "longer"));
+        input = "    test   ";
+        strip_whitespace(input);
+        CPPUNIT_ASSERT_EQUAL(std::string("test"), input);
 
-    // empty strings don't start with anything
-    CPPUNIT_ASSERT_EQUAL(false, starts_with("", "foo"));
+        input = "\r\n  test\r\n";
+        strip_whitespace(input);
+        CPPUNIT_ASSERT_EQUAL(std::string("test"), input);
+    }
+    void testStartsWith()
+    {
+        CPPUNIT_ASSERT_EQUAL(true, starts_with("preteststring", "pre"));
+        CPPUNIT_ASSERT_EQUAL(true, starts_with("pre", "pre"));
+        CPPUNIT_ASSERT_EQUAL(false, starts_with("teststring", "pre"));
+        CPPUNIT_ASSERT_EQUAL(false, starts_with("long", "longer"));
 
-    // but everything starts with the empty string
-    CPPUNIT_ASSERT_EQUAL(true, starts_with("foo", ""));
-    CPPUNIT_ASSERT_EQUAL(true, starts_with("", ""));
-}
-void TestStrUtil::testEndsWith()
-{
-    CPPUNIT_ASSERT_EQUAL(true, ends_with("teststringpost", "post"));
-    CPPUNIT_ASSERT_EQUAL(true, ends_with("post", "post"));
-    CPPUNIT_ASSERT_EQUAL(false, ends_with("teststring", "post"));
+        // empty strings don't start with anything
+        CPPUNIT_ASSERT_EQUAL(false, starts_with("", "foo"));
 
-    // empty strings don't end with anything
-    CPPUNIT_ASSERT_EQUAL(false, ends_with("", "foo"));
+        // but everything starts with the empty string
+        CPPUNIT_ASSERT_EQUAL(true, starts_with("foo", ""));
+        CPPUNIT_ASSERT_EQUAL(true, starts_with("", ""));
+    }
+    void testEndsWith()
+    {
+        CPPUNIT_ASSERT_EQUAL(true, ends_with("teststringpost", "post"));
+        CPPUNIT_ASSERT_EQUAL(true, ends_with("post", "post"));
+        CPPUNIT_ASSERT_EQUAL(false, ends_with("teststring", "post"));
 
-    // but everything ends with the empty string
-    CPPUNIT_ASSERT_EQUAL(true, ends_with("foo", ""));
-    CPPUNIT_ASSERT_EQUAL(true, ends_with("", ""));
-}
+        // empty strings don't end with anything
+        CPPUNIT_ASSERT_EQUAL(false, ends_with("", "foo"));
+
+        // but everything ends with the empty string
+        CPPUNIT_ASSERT_EQUAL(true, ends_with("foo", ""));
+        CPPUNIT_ASSERT_EQUAL(true, ends_with("", ""));
+    }
+};
+
 CPPUNIT_TEST_SUITE_REGISTRATION(TestStrUtil);
 
