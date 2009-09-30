fefefd322385f53c4d89c47bd2f89e43dcf6e32a
nicolas.alvarez <nicolas.alvarez@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2009-09-30 16:31:40 +0000

    Remove my name from the TestSkeleton. It should be replaced by whoever uses it.

--- trunk/tests/TestSkeleton.cpp
+++ trunk/tests/TestSkeleton.cpp
@@ -1,6 +1,6 @@
 // This file is part of Synecdoche.
 // http://synecdoche.googlecode.com/
-// Copyright (C) 2009 Nicolas Alvarez
+// Copyright (C) 2009 <Your name here>
 //
 // Synecdoche is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published
