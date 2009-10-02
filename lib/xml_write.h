d1b2c14a3c152a3e4196e4c283830661c5df0900
nicolas.alvarez <nicolas.alvarez@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2009-10-02 23:17:30 +0000

    Add xml_write.h with helpers to serialize XML and escape strings. Add unit tests for it. Modify a part of gui_rpc_server_ops to use this new code.

---
+++ lib/xml_write.h
@@ -0,0 +1,83 @@
+// This file is part of Synecdoche.
+// http://synecdoche.googlecode.com/
+// Copyright (C) 2009 Nicolas Alvarez
+//
+// Synecdoche is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Synecdoche is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License with Synecdoche.  If not, see <http://www.gnu.org/licenses/>.
+
+/// \file
+/// \todo document!
+
+#ifndef XML_WRITE_H
+#define XML_WRITE_H
+
+#include <cstdlib>
+#include <ostream>
+#include <sstream>
+
+template <typename T>
+class XmlTag_t {
+private:
+    const char* name;
+    const T& value;
+
+    friend std::ostream& operator<<(std::ostream& stream, const XmlTag_t<T>& tag) {
+        return stream << '<' << tag.name << '>' << tag.value << "</" << tag.name << ">\n";
+    }
+public:
+    XmlTag_t(const char* name, const T& value):
+        name(name), value(value)
+    {}
+};
+
+template <typename T>
+XmlTag_t<T> XmlTag(const char* name, const T& value) {
+    return XmlTag_t<T>(name, value);
+}
+
+inline void write_escaped_xml(std::ostream& stream, const char* str) {
+    for (; *str; ++str) {
+        int c = (unsigned char)*str;
+        if (c == '<') {
+            stream << "&lt;";
+        } else if (c == '&') {
+            stream << "&amp;";
+        } else if (c > 127) {
+            stream << "&#" << c << ';';
+        } else if (c < 32) {
+            switch(c) {
+            case 9:
+            case 10:
+            case 13:
+                stream << "&#" << c << ';';
+            }
+        } else {
+            stream.put(c);
+        }
+    }
+}
+class XmlString {
+private:
+    const char* data;
+public:
+    XmlString(const char* data): data(data){}
+    XmlString(const std::string& str): data(str.c_str()){}
+
+    friend std::ostream& operator<<(std::ostream& stream, const XmlString& str) {
+        write_escaped_xml(stream, str.data);
+        return stream;
+    }
+};
+
+
+#endif
---
+++ tests/TestXmlWrite.cpp
@@ -0,0 +1,137 @@
+// This file is part of Synecdoche.
+// http://synecdoche.googlecode.com/
+// Copyright (C) 2009 Nicolas Alvarez
+//
+// Synecdoche is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Synecdoche is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License with Synecdoche.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/TestAssert.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+#include <string>
+#include <sstream>
+#include "lib/xml_write.h"
+
+using std::string;
+
+/// Unit tests for the write_escaped_xml function.
+class TestXmlEscape: public CppUnit::TestFixture
+{
+    CPPUNIT_TEST_SUITE(TestXmlEscape);
+    CPPUNIT_TEST(testNothing);
+    CPPUNIT_TEST(testEmpty);
+    CPPUNIT_TEST(testAmp);
+    CPPUNIT_TEST(testTag);
+    CPPUNIT_TEST(testConsecutive);
+    CPPUNIT_TEST(testHighByte);
+    CPPUNIT_TEST_SUITE_END();
+
+  private:
+    std::ostringstream oss;
+
+  public:
+//these macros makes data-driven tests easier
+#define ADD_TEST(name, input, output) \
+void test ## name() { \
+    write_escaped_xml(oss, input); \
+    CPPUNIT_ASSERT_EQUAL(string(output), oss.str()); \
+}
+#define ADD_TEST2(name, input, output1, output2) \
+void test ## name() { \
+    write_escaped_xml(oss, input); \
+    try { \
+        CPPUNIT_ASSERT_EQUAL(string(output1), oss.str()); \
+    } catch (CppUnit::Exception) { \
+        CPPUNIT_ASSERT_EQUAL(string(output2), oss.str()); \
+    } \
+}
+
+    /// Test an input that doesn't need escaping.
+    /// The output should be the same as the input.
+    ADD_TEST(Nothing, "test", "test")
+
+    /// Test an empty string as input.
+    ADD_TEST(Empty, "", "")
+
+    /// Test escaping a string containing an ampersand.
+    ADD_TEST(Amp, "t&st", "t&amp;st")
+
+    /// Test escaping a typical XML tag. This test checks if the open angle
+    /// bracket is escaped, but works both if the right angle bracket is
+    /// escaped and if it's not.
+    ADD_TEST2(Tag, "<test>", "&lt;test&gt;", "&lt;test>")
+
+    /// Test two consecutive ampersands.
+    ADD_TEST(Consecutive, "x&&x", "x&amp;&amp;x")
+
+    /// Test a string with a character &gt;128.
+    ADD_TEST(HighByte, "qu\xe9?", "qu&#233;?")
+#undef ADD_TEST
+#undef ADD_TEST2
+};
+
+/// Unit tests for the XmlTag_t class, and using it along with XmlString.
+class TestXmlWrite: public CppUnit::TestFixture
+{
+    CPPUNIT_TEST_SUITE(TestXmlWrite);
+    CPPUNIT_TEST(testInt);
+    CPPUNIT_TEST(testStrLiteral);
+    CPPUNIT_TEST(testCharp);
+    CPPUNIT_TEST(testStdString);
+
+    CPPUNIT_TEST(testStrLiteralEsc);
+    CPPUNIT_TEST(testCharpEsc);
+    CPPUNIT_TEST(testStdStringEsc);
+    CPPUNIT_TEST_SUITE_END();
+
+  private:
+    std::ostringstream oss;
+  public:
+    void testInt() {
+        oss << XmlTag("foo", 42);
+        CPPUNIT_ASSERT_EQUAL(string("<foo>42</foo>\n"), oss.str());
+    }
+    void testStrLiteral() {
+        oss << XmlTag("foo", "bar");
+        CPPUNIT_ASSERT_EQUAL(string("<foo>bar</foo>\n"), oss.str());
+    }
+    void testCharp() {
+        const char* value = "bar";
+        oss << XmlTag("foo", value);
+        CPPUNIT_ASSERT_EQUAL(string("<foo>bar</foo>\n"), oss.str());
+    }
+    void testStdString() {
+        string value = "bar";
+        oss << XmlTag("foo", value);
+        CPPUNIT_ASSERT_EQUAL(string("<foo>bar</foo>\n"), oss.str());
+    }
+    void testStrLiteralEsc() {
+        oss << XmlTag("foo", XmlString("b&r"));
+        CPPUNIT_ASSERT_EQUAL(string("<foo>b&amp;r</foo>\n"), oss.str());
+    }
+    void testCharpEsc() {
+        const char* value = "b&r";
+        oss << XmlTag("foo", XmlString(value));
+        CPPUNIT_ASSERT_EQUAL(string("<foo>b&amp;r</foo>\n"), oss.str());
+    }
+    void testStdStringEsc() {
+        string value = "b&r";
+        oss << XmlTag("foo", XmlString(value));
+        CPPUNIT_ASSERT_EQUAL(string("<foo>b&amp;r</foo>\n"), oss.str());
+    }
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TestXmlEscape);
+CPPUNIT_TEST_SUITE_REGISTRATION(TestXmlWrite);
+
