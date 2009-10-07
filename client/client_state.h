6ceb785fb86502a7c7f7a2764415bc6d8cb44b43
nicolas.alvarez <nicolas.alvarez@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2009-10-07 21:06:30 +0000

    Convert read_trickle_files to use iostreams.

--- client/client_state.h
+++ client/client_state.h
@@ -414,7 +414,7 @@ public:
 /// @name cs_trickle.C
 private:
     /// Scan project dir for trickle files and convert them to XML.
-    int read_trickle_files(const PROJECT* project, MIOFILE& f);
+    int read_trickle_files(const PROJECT* project, std::ostream& out);
 
     /// Remove trickle files when ack has been received.
     int remove_trickle_files(const PROJECT* project);
