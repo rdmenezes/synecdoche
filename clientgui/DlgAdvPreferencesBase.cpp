7450761fcfee5a096aafbee2183a44120de9dc74
nicolas.alvarez <nicolas.alvarez@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2010-05-29 03:20:18 +0000

    Add copyright/license comment header to DlgAdvPreferencesBase.{cpp,h}.

--- clientgui/DlgAdvPreferencesBase.cpp
+++ clientgui/DlgAdvPreferencesBase.cpp
@@ -1,9 +1,25 @@
-///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Oct 13 2006)
-// http://www.wxformbuilder.org/
+// This file is part of Synecdoche.
+// http://synecdoche.googlecode.com/
+// Copyright (C) 2010 Nicolas Alvarez
+// Copyright (C) 2010 University of California
 //
-// PLEASE DO "NOT" EDIT THIS FILE!
-///////////////////////////////////////////////////////////////////////////
+// Synecdoche is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Synecdoche is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License with Synecdoche.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+
+// This code was initially generated with wxFormBuilder (version Oct 13 2006)
+// http://www.wxformbuilder.org/
 
 #include "DlgAdvPreferencesBase.h"
 
