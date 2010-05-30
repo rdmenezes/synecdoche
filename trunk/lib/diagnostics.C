28a0a568110aaa9f34b098d44d8e6aacb954e4d4
DerMeister667 <DerMeister667@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2010-05-30 15:38:56 +0000

    Applied BOINC changeset 18959: Fixed problem that prevented stack backtrace from being printed after segfault on linux.

--- trunk/lib/diagnostics.C
+++ trunk/lib/diagnostics.C
@@ -1,7 +1,7 @@
 // This file is part of Synecdoche.
 // http://synecdoche.googlecode.com/
 // Copyright (C) 2008 David Barnard
-// Copyright (C) 2005 University of California
+// Copyright (C) 2009 University of California
 //
 // Synecdoche is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published
@@ -558,7 +558,21 @@ void boinc_catch_signal(int signal) {
     void *array[64];
     size_t size;
     size = backtrace (array, 64);
-    fprintf(stderr, "Stack trace (%lu frames):\n", size);
+    //  Anything that calls malloc here (i.e *printf()) will probably fail 
+    //  so we'll do it the hard way. 
+    write(fileno(stderr), "Stack trace (", strlen("Stack trace (")); 
+    char mbuf[10]; 
+    char* p = mbuf + 9; 
+    int i = size; 
+    *(p--) = 0; 
+    while (i) { 
+        *(p--) = i % 10 + '0'; 
+        i /= 10; 
+    } 
+    write(fileno(stderr), p + 1, strlen(p + 1)); 
+    write(fileno(stderr), " frames):", strlen(" frames):")); 
+    mbuf[0] = 10; 
+    write(fileno(stderr), mbuf, 1); 
     backtrace_symbols_fd(array, size, fileno(stderr));
 #endif
 
