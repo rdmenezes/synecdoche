c138c1527e8418c44ddbe262ca1447a8182f8d8d
nicolas.alvarez <nicolas.alvarez@a95a1d4e-06cc-11df-af7f-9df10b504a6d>
2010-06-15 02:22:48 +0000

    Add unit tests for buildLayout.

---
+++ trunk/clientgui/tests/TestBuildLayout.cpp
@@ -0,0 +1,85 @@
+// This file is part of Synecdoche.
+// http://synecdoche.googlecode.com/
+// Copyright (C) 2010 Nicolas Alvarez
+//
+// Synecdoche is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Synecdoche is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License with Synecdoche.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <UnitTest++.h>
+
+#include <wx/frame.h>
+#include <wx/sizer.h>
+#include <wx/checkbox.h>
+#include <wx/textctrl.h>
+#include <wx/stattext.h>
+
+#include "clientgui/BuildLayout.h"
+
+class MainWindow: public wxFrame {
+public:
+    MainWindow(): wxFrame(NULL, wxID_ANY, wxT("test")) {}
+};
+
+/// This test simply checks of MoveBeforeInTabOrder works as I expect it to work.
+/// It doesn't test any of our code.
+TEST(WxTabOrder)
+{
+    MainWindow window;
+    wxBoxSizer* dialogSizer = new wxBoxSizer( wxHORIZONTAL );
+
+    wxCheckBox* chkFoo = new wxCheckBox(&window, wxID_ANY, wxT("Foo"));
+    wxCheckBox* chkBar = new wxCheckBox(&window, wxID_ANY, wxT("Bar"));
+    dialogSizer->Add(chkFoo);
+    dialogSizer->Add(chkBar);
+    chkBar->MoveBeforeInTabOrder(chkFoo);
+    window.SetSizer(dialogSizer);
+
+    // controls get order changed in the list of children of the parent window
+    // according to the modified tab order
+    const wxWindowList& windowList = window.GetChildren();
+    CHECK_EQUAL(2, windowList.size());
+    CHECK_EQUAL(chkBar, windowList[0]);
+    CHECK_EQUAL(chkFoo, windowList[1]);
+
+    // but they keep their visual order in the sizer
+    const wxSizerItemList& sizerList = dialogSizer->GetChildren();
+    CHECK_EQUAL(2, sizerList.size());
+    CHECK_EQUAL(chkFoo, sizerList[0]->GetWindow());
+    CHECK_EQUAL(chkBar, sizerList[1]->GetWindow());
+}
+SUITE(TestBuildLayout)
+{
+    TEST(SimpleCase) {
+        MainWindow window;
+        wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
+        wxTextCtrl* textBox = new wxTextCtrl(&window, wxID_ANY);
+
+        buildLayout(&window, sizer, wxT("Use up to %1 threads"), textBox);
+
+        const wxSizerItemList& sizerList = sizer->GetChildren();
+        CHECK_EQUAL(3, sizerList.size());
+
+        // left label
+        wxStaticText* label1 = wxDynamicCast(sizerList[0]->GetWindow(), wxStaticText);
+        CHECK(label1);
+        CHECK_EQUAL("Use up to", (const char*)label1->GetLabel().ToAscii());
+
+        // text field
+        CHECK_EQUAL(sizerList[1]->GetWindow(), textBox);
+
+        // right label
+        wxStaticText* label2 = wxDynamicCast(sizerList[2]->GetWindow(), wxStaticText);
+        CHECK(label2);
+        CHECK_EQUAL("threads", (const char*)label2->GetLabel().ToAscii());
+    }
+}
